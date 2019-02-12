de74719530fb7a13a5a24e07f75d41839959eaec
SergGlinyanov <45871374+SergGlinyanov@users.noreply.github.com>
2019-02-12 10:46:17 +0400

    Initial commit

---
+++ My web app/src/main/java/org/mycompany/myname/HelloServlet.java
@@ -0,0 +1,11 @@
+package org.mycompany.myname;
+
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class HelloServlet extends HttpServlet {
+    public void doGet(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse)
+            throws IOException {
+        httpServletResponse.getWriter().print("Hello from servlet");
+    }
+}
